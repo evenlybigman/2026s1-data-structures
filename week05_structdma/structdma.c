854c9fdf1c6014012cc9a0d0ed25bbf007f4e351
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-01 00:31:03 +0900

    struct 객체에 포인터 사용법

---
+++ week05_structdma/structdma.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct studentTag {
+	char name[10];
+	int age;
+	double pa;
+} student;
+
+int main(void) {
+	student* p;
+	p = (student*)malloc(sizeof(student));
+	if (p == NULL) {
+		fprintf(stderr, "메모리가 부족해서 할당할 수 없습니다.");
+		exit(1);
+	}
+	strcpy(p->name, "Park");
+	p->age = 20;
+	free(p);
+	return 0;
+}

b6f98974b17ebebd2006128a1d8f419af0412728
Eve <evenlybigman@gmail.com>
2026-04-21 22:43:38 +0900

    수업 복습

---
+++ CodePractice/CodePractice/hanoitop.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+
+void hanoi_tower(int n, char from, char tmp, char to) {
+	int count = 0;
+	if (n <= 1) return;
+	else {
+		hanoi_tower(n - 1, from, to, tmp);
+		printf("%c를 %c로 옮겼습니다.\n", from, to);
+		hanoi_tower(n-1, from, tmp, to);
+		
+	}
+}
+
+
+int main() {
+	hanoi_tower(4, 'a','b','c');
+}

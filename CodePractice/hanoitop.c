c1747990497c3976d0dad05c17fb3036ebbc03d8
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-22 00:04:57 +0900

    하노이탑 수정

--- CodePractice/hanoitop.c
+++ CodePractice/hanoitop.c
@@ -1,10 +1,5 @@
 ﻿#include <stdio.h>
 
-//하노이 탑
-//n개의 탑을 start에서 end로 옮긴다.
-//n = 1일때 n을 end로 옮긴다.
-//n-1을 임시 공간으로 옮긴다. n을 end로 옮긴다.
-//n-1을 end로 옮긴다.
 void hanoi_tower(int n, char start, char tmp, char end) {
 	if (n <= 0) return;
 	else if (n == 1) {
@@ -16,9 +11,6 @@ void hanoi_tower(int n, char start, char tmp, char end) {
 		hanoi_tower(n - 1, tmp, start, end);
 	}
 }
-
-
-
 
 int main() {
 	hanoi_tower(4, 'a', 'b', 'c');

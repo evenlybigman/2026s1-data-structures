bd7d69b913d9d0c38d4b242f9c3eb2b794aeef39
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-22 00:04:22 +0900

    하노이탑 수정

--- CodePractice/hanoitop.c
+++ CodePractice/hanoitop.c
@@ -8,11 +8,11 @@
 void hanoi_tower(int n, char start, char tmp, char end) {
 	if (n <= 0) return;
 	else if (n == 1) {
-		printf("원판 %d를 %c에서 %c로 옮긴다.\n", start, end);
+		printf("원판 %d를 %c에서 %c로 옮긴다.\n",n, start, end);
 	}
 	else {
 		hanoi_tower(n - 1, start, end, tmp);
-		printf("원판 %d를 %c에서 %c로 옮긴다.\n", start, end);
+		printf("원판 %d를 %c에서 %c로 옮긴다.\n",n, start, end);
 		hanoi_tower(n - 1, tmp, start, end);
 	}
 }

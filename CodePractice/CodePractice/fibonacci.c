0c6cc1f65c1033b0c1e8a6ef0f27680920e335b0
Eve <evenlybigman@gmail.com>
2026-04-21 20:13:50 +0900

    v

---
+++ CodePractice/CodePractice/fibonacci.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+
+int fibonacci(int n) {
+	if (n <= 1) return n;
+	else return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+//반복문을 이용한 피보나치 수열
+int fibonacci_iter(int n) {
+	int first = 0, second = 1, next = 0;
+	if (n <= 1) return n;
+	for (int i = 2; i <= n; i++) { // 2
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	return next;
+}
+
+int main() {
+	printf("%d\n", fibonacci_iter(8));
+	printf("%d", fibonacci(7));
+}

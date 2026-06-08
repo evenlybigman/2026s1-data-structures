2a7c843fd745f0f7c6c7320c5f20cd87d30b8f70
Eve <evenlybigman@gmail.com>
2026-06-08 21:55:03 +0900

    스택을 이용한 중위 순회 구현

---
+++ week11/stackTraversal/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+typedef int element;
+
+typedef struct TreeNode {
+	element data;
+	struct treeNode* left, * right;
+}TreeNode;
+
+//       15
+//     4  20
+//   1  16  25
+
+TreeNode n1 = { 1, NULL, NULL };
+TreeNode n2 = { 4,  &n1, NULL };
+TreeNode n3 = { 16, NULL, NULL };
+TreeNode n4 = { 25, NULL, NULL };
+TreeNode n5 = { 20,  &n3,  &n4 };
+TreeNode n6 = { 15,  &n2,  &n5 };
+TreeNode* root = &n6;
+
+#define SIZE 100
+int top = -1;
+TreeNode* stack[SIZE];
+
+void push(TreeNode* p) {
+	if (top < SIZE - 1) {
+		stack[++top] = p;
+	}
+}
+
+TreeNode* pop() {
+	if (top > -1) {
+		return stack[top--];
+	}
+}
+
+void inorder_iter(TreeNode* root) {
+	while (1) {
+		for (; root; root = root->left) {
+			push(root);
+		}
+		root = pop();
+		if (!root) break;
+		printf("[%d] ", root->data);
+		root = root->right;
+	}
+}
+
+int main() {
+	printf("중위 순회= ");
+	inorder_iter(root);
+	printf("\n");
+}
